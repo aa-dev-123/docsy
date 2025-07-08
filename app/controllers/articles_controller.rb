34c6629a366d075f554204cf71f9b3123701b615
Alishba <alishbaather321@gmail.com>
2025-07-08 19:05:56 +0500

    Added Article Controller actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,31 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: "Article was successfully created."
+    else
+      render :new, alert: "Article could not be created."
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :description,
+      :price,
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   #        :recoverable, :rememberable, :validatable
 
   devise :database_authenticatable, :registerable
+
+  has_many :articles
 end
