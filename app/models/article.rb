054476038c0f5b5d18cb92ddd8fe2f293c1ec7b7
Alishba <alishbaather321@gmail.com>
2025-07-18 20:08:42 +0500

    real-time broadcasting for the new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,6 @@ class Article < ApplicationRecord
   validates :title, :description, :price, presence: true
 
   default_scope { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "articles", partial: "articles/article", locals: { article: self }, target: "articles" }
 end
