3a859a9a5f9c119c41473472ed13cf5d59b31c1c
Alishba <alishbaather321@gmail.com>
2025-07-18 20:17:00 +0500

    Real-time updates to the update and destroy actions

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,6 @@ class Article < ApplicationRecord
   default_scope { order(id: :desc) }
 
   after_create_commit -> { broadcast_prepend_to "articles", partial: "articles/article", locals: { article: self }, target: "articles" }
+  after_update_commit -> { broadcast_replace_to "articles", partial: "articles/article", locals: { article: self } } # DEFAULT target is model.plural which is definately the case here
+  after_destroy_commit -> { broadcast_remove_to "articles" }
 end
